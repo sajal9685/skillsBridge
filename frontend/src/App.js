7b33155fb5d20f902d6e9e8d877ec4767fb56f82
Sajal Chaturvedi <sajalchaturvedi8@gmail.com>
2025-05-26 22:13:53 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Quiz from './pages/Quiz';
-import ResumeBuilder from './pages/ResumeBuilder';
-import Roadmap from './pages/Roadmap';
-import Mentorship from './pages/Mentorship';
-import Navbar from './components/Navbar';
+
+// import Home from './pages/Home';
+// import Quiz from './pages/Quiz';
+// import ResumeBuilder from './pages/ResumeBuilder';
+// import Roadmap from './pages/Roadmap';
+// import Mentorship from './pages/Mentorship';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
-    <Router>
+    <div>
       <Navbar />
-      <div className="p-4">
+      {/* <div className="p-4">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/quiz" element={<Quiz />} />
           <Route path="/resume" element={<ResumeBuilder />} />
           <Route path="/roadmap" element={<Roadmap />} />
           <Route path="/mentorship" element={<Mentorship />} />
-        </Routes>
+        </Routes> */}
       </div>
-    </Router>
+    // </Router>
   );
 }
 
